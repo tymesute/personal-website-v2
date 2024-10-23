8be6f28b40b14ccad3b291dda8fcfd64d4ac5a68
Tymeee <tyme.suteesopon@gmail.com>
2024-10-23 18:55:32 -0400

    images

--- src/Pages/Content.jsx
+++ src/Pages/Content.jsx
@@ -38,15 +38,15 @@ export default function Content() {
               // implement mapping to image: https://cedricdelpoux.github.io/react-responsive-masonry/
             }
             <Masonry gutter="10px">
-              <img src={"src/assets/tyme-pic2.png"} />
-              <img src={"src/assets/img_4923.JPG"} />
-              <img src={"src/assets/img_7072.JPG"} />
-              <img src={"src/assets/img_1788.JPG"} />
-              <img src={"src/assets/img_9844.JPG"} />
-              <img src={"src/assets/mocha1.png"} />
-              <img src={"src/assets/img_6785.JPG"} />
-              <img src={"src/assets/img_4836.JPG"} />
-              <img src={"src/assets/friends1.png"} />
+              <img src={"assets/tyme-pic2.png"} />
+              <img src={"assets/img_4923.JPG"} />
+              <img src={"assets/img_7072.JPG"} />
+              <img src={"assets/img_1788.JPG"} />
+              <img src={"assets/img_9844.JPG"} />
+              <img src={"assets/mocha1.png"} />
+              <img src={"assets/img_6785.JPG"} />
+              <img src={"assets/img_4836.JPG"} />
+              <img src={"assets/friends1.png"} />
             </Masonry>
           </ResponsiveMasonry>
         </div>
