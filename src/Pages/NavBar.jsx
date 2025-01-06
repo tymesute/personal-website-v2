ce972211e87d9efeb1ce38156d6a212e2dfbdd1c
Tymeee <tyme.suteesopon@gmail.com>
2025-01-06 08:56:25 -0500

    edit nav bar

--- src/Pages/NavBar.jsx
+++ src/Pages/NavBar.jsx
@@ -5,13 +5,13 @@ export default function NavBar() {
         <a href="#about">About</a>
         <a href="#projects">Experience</a>
         <a href="#contact">Contact</a>
-        <a
+        {/* <a
           style={{ marginLeft: "auto" }}
           href="https://drive.google.com/file/d/1KkAXpwUeRgMZN4Dxrb9b6vQfPuKlQivi/view?usp=sharing"
           target="_blank"
         >
           Resume
-        </a>
+        </a> */}
       </nav>
     </header>
   );
